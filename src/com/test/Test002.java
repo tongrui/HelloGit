5f12c66ef11a5a5d9edf95950ed49117cd90d6ab
rui.tong <rui.tong@dianping.com>
2014-09-21 09:40:07 +0800

    0922

--- src/com/test/Test002.java
+++ src/com/test/Test002.java
@@ -4,7 +4,7 @@ public class Test002 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("111");
+        System.out.println("0922");
     }
 
 }

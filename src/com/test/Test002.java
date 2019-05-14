f4c3dea470bd4fb23a7cd4993d4d90112cac390d
tongrui120 <tongrui120@126.com>
2019-05-14 18:01:55 +0800

    test commit

--- src/com/test/Test002.java
+++ src/com/test/Test002.java
@@ -4,6 +4,7 @@ public class Test002 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        //test commit
         System.out.println("1001");
     }
 
